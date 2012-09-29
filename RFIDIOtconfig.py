46b766a802d1248aa804a9ba961aec97e6700500
Adam Laurie <adam@algroup.co.uk>
2012-09-29 17:57:14 +0100

    don't default to Android device

--- RFIDIOtconfig.py
+++ RFIDIOtconfig.py
@@ -66,10 +66,10 @@ line= "/dev/ttyUSB0"
 #readertype= RFIDIOt.rfidiot.READER_FROSCH
 #readertype= RFIDIOt.rfidiot.READER_DEMOTAG
 # READER_PCSC is a meta type. Actual subtype will be auto-determined.
-#readertype= RFIDIOt.rfidiot.READER_PCSC
+readertype= RFIDIOt.rfidiot.READER_PCSC
 #readertype= RFIDIOt.rfidiot.READER_NONE
 #readertype= RFIDIOt.rfidiot.READER_LIBNFC
-readertype= RFIDIOt.rfidiot.READER_ANDROID
+#readertype= RFIDIOt.rfidiot.READER_ANDROID
 
 # PCSC reader number (can be overridden with -r)
 readernum= 0
