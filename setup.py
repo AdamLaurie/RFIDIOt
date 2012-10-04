33e6ec8012f7abc399ba1f6f7cb752fca46e23f9
Adam Laurie <adam@algroup.co.uk>
2012-10-04 12:58:48 +0100

    add ACG & Frosch test scripts to setup

--- setup.py
+++ setup.py
@@ -1,16 +1,14 @@
-#import sys
-#import os
 from distutils.core import setup, Extension
 
 packages= ['rfidiot']
 
-scripts = ['pynfc.py', 'cardselect.py', 'ChAP.py', 'copytag.py', 'demotag.py', 
+scripts = ['cardselect.py', 'ChAP.py', 'copytag.py', 'demotag.py', 
 	   'eeprom.py', 'fdxbnum.py', 'formatmifare1kvalue.py', 'froschtest.py', 'hidprox.py', 'hitag2brute.py',
 	   'hitag2reset.py', 'iso3166.py', 'isotype.py', 'jcopmifare.py', 'jcopsetatrhist.py', 'jcoptool.py',
 	   'lfxtype.py', 'loginall.py', 'mifarekeys.py', 'mrpkey.py', 'multiselect.py', 'pn532emulate.py', 
-	   'pn532mitm.py', 'pn532.py', 'pyandroid.py', 'q5reset.py', 'readlfx.py', 'readmifare1k.py', 
+	   'pn532mitm.py', 'pn532.py', 'q5reset.py', 'readlfx.py', 'readmifare1k.py', 
 	   'readmifaresimple.py', 'readmifareultra.py', 'readtag.py', 'send_apdu.py', 'sod.py', 'transit.py',
-	   'unique.py', 'writelfx.py', 'writemifare1k.py'
+	   'unique.py', 'writelfx.py', 'writemifare1k.py', 'testacg.sh', 'testlahf.sh'
 	]
 
 setup  (name        = 'rfidiot',
